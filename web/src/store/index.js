839c901e49916eea0d42f954a898d4eac3f42492
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-01-31 17:07:52 -0800

    adds react boilerplate

---
+++ web/src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from '../reducers'
+
+const middleware = [thunk]
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore)
+const store = createStoreWithMiddleware(reducers)
+
+export default store
